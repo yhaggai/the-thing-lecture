fbeeeb6137734115a97fa4e216597037458d341e
Haggai Yaniv <yhaggai@gmail.com>
2026-06-13 17:23:02 +0300

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact({ compat: true })],
+  site: 'https://the-thing.click',
+  integrations: [preact({ compat: true }), sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
